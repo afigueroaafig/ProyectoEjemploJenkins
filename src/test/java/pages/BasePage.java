5893f79b096d173b35f300343c439fea3f417d94
afigueroaafig <134719601+afigueroaafig@users.noreply.github.com>
2023-06-28 14:13:25 -0400

    Update BasePage.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -29,14 +29,14 @@ static{
     //System.setProperty("webdriver.chrome.driver","C:\\selenium_webdriver\\chromedriver.exe");
     //System.setProperty("webdriver.chrome.driver","C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\SeleniumGoogle2\\chromedriver.exe");
 
-    FirefoxOptions fireFoxOptions = new FirefoxOptions();
-    driver = new FirefoxDriver(fireFoxOptions);
+    //FirefoxOptions fireFoxOptions = new FirefoxOptions();
+    //driver = new FirefoxDriver(fireFoxOptions);
 
     //EdgeOptions edgeOptions = new EdgeOptions();
     //driver = new EdgeDriver(edgeOptions);
 
-    //ChromeOptions chromeOptions = new ChromeOptions();
-    //driver = new ChromeDriver(chromeOptions);
+    ChromeOptions chromeOptions = new ChromeOptions();
+    driver = new ChromeDriver(chromeOptions);
 
 
     wait = new WebDriverWait(driver, Duration.ofSeconds(10));
