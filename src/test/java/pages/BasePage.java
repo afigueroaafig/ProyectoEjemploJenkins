0cba19c71f33dc9b94a68f1c02818c165496dbe6
Alvaro <you@example.com>
2023-06-28 13:37:24 -0400

    modoficacion de basepage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -6,6 +6,11 @@ import java.time.Duration;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.Select;
@@ -17,13 +22,22 @@ public class BasePage {
 
 static{
 
-    System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe"); 
+    //System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe"); 
+
+
     //System.setProperty("webdriver.chrome.driver","C:\\Users\\Home\\eclipse-workspace\\ProyectoEjemploJenkins2\\driver\\chromedriver.exe");
     //System.setProperty("webdriver.chrome.driver","C:\\selenium_webdriver\\chromedriver.exe");
     //System.setProperty("webdriver.chrome.driver","C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\SeleniumGoogle2\\chromedriver.exe");
 
-    ChromeOptions chromeOptions = new ChromeOptions();
-    driver = new ChromeDriver(chromeOptions);
+    //FirefoxOptions fireFoxOptions = new FirefoxOptions();
+    //driver = new FirefoxDriver(fireFoxOptions);
+
+    EdgeOptions edgeOptions = new EdgeOptions();
+    driver = new EdgeDriver(edgeOptions);
+
+    //ChromeOptions chromeOptions = new ChromeOptions();
+    //driver = new ChromeDriver(chromeOptions);
+
     wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
